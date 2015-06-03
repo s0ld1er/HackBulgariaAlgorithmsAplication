7f816472cb41ad29c0b72bd4c307346c0d06a59f
Jivko <s0ld1er_pz@abv.bg>
2015-06-03 08:17:09 +0300

    Fixed names

--- src/PalindromeImpl.java
+++ src/PalindromeImpl.java
@@ -1,6 +1,53 @@
-public interface PalindromeImpl {
-    public boolean isPalindrome(final String argument);
+/**
+ * This is a simple program to find all palindrome rotations of given string
+ * 
+ * @author Jivko Todorov
+ * @see TestClass.java for unit tests
+ * @return String with the answer of all rotations or "NONE" if there are none
+ *         existent
+ */
 
-    public String allRotations(final String inputWord);
+public class PalindromeImpl implements Palindrome {
+    public PalindromeImpl() {
+    }
 
+    /*
+     * The method isPalindrome checks if a given string is a lexicographical
+     * palindrome or not
+     */
+
+    public final boolean isPalindrome(String argument) {
+        for (int i = 0; i < argument.length() >> 1; i++) {
+            if (argument.charAt(i) != argument.charAt(argument.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public final String allRotations(final String inputWord) {
+        StringBuilder finalAnswer = new StringBuilder();
+        StringBuilder currentWord = new StringBuilder();
+
+        // initial check if the given word is a palindrome - add it to the
+        // answer string
+        if (isPalindrome(inputWord)) {
+            finalAnswer.append(inputWord).append(System.lineSeparator());
+        }
+
+        // i have observated that if we rotate the string and check first and
+        // last chars we are actually checking the i-th and the i+1 -th char
+        // which can help us reduce our complexity
+        for (int i = inputWord.length() - 1; i > 0; i--) {
+            if (inputWord.charAt(i) == inputWord.charAt(i - 1)) {
+                currentWord.append(inputWord.substring(i)).append(inputWord.substring(0, i));
+                if (isPalindrome(currentWord.toString())) {
+                    finalAnswer.append(currentWord).append(System.lineSeparator());
+                }
+                currentWord.setLength(0);
+            }
+        }
+
+        return finalAnswer.length() > 0 ? finalAnswer.toString() : "NONE";
+    }
 }
